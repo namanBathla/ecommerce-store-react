1a61c0a478c33552b09cde44d89edf4918bc5f63
Naman <namanuser1307@gmail.com>
2025-06-16 00:50:04 +0530

    added untracked files

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,80 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import NotFound from './pages/NotFound';
-import Home from './pages/Home';
-import About from './pages/About'
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
-import Signup from './pages/Signup';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
     },
     {
       path: "/about",
-      element: <About/>
+      element: (
+        <>
+          <Navbar />
+          <About />
+        </>
+      ),
     },
     {
       path: "/login",
-      element: <Login/>
+      element: (
+        <>
+          <Navbar />
+          <Login />
+        </>
+      ),
     },
     {
       path: "/signup",
-      element: <Signup/>
+      element: <Signup />,
     },
     {
       path: "/cart",
-      element: <Cart/>
+      element: (
+        <>
+          <Navbar />
+          <Cart />
+        </>
+      ),
     },
     {
       path: "/profile/:username",
-      element: <Profile/>
+      element: (
+        <>
+          <Navbar />
+          <Profile />
+        </>
+      ),
     },
     {
       path: "/*",
-      element: <NotFound/>
-    }
+      element: (
+        <>
+          <Navbar />
+          <NotFound />
+        </>
+      ),
+    },
   ]);
   return (
     <>
-      <RouterProvider router={router}/>
-      <Navbar/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
