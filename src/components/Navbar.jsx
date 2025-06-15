2a8322fbf93ffefbc2c0426bc5e61ebdc019032b
Naman <namanuser1307@gmail.com>
2025-06-16 00:47:51 +0530

    Debug Routing and added Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import '../App.css';
 
 function Navbar() {
   return (
-    <div>
+    <div className="flex gap-2 w-full">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/profile/user1">User</NavLink>
